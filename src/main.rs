b22bc685a45f1720e2973c30d497134fe59ea4a8
Tony George <tg764562@gmail.com>
2025-06-08 15:17:17 +0300

    feat: improve undefined commands log

--- src/main.rs
+++ src/main.rs
@@ -9,5 +9,5 @@ fn main() {
     // Wait for user input
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
-    println!("{}: command not found", input.trim());
+    println!("{}: command not found", input.split_whitespace().next().unwrap());
 }
