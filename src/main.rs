84e2f4ae1cd554118c02aa14001946973d10a108
Tony George <tg764562@gmail.com>
2025-06-08 15:14:47 +0300

    feat: handle undefined commands

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@ fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
 
+    let stdin = io::stdin();
     // Wait for user input
     let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
+    stdin.read_line(&mut input).unwrap();
+    println!("{}: command not found", input.trim());
 }
