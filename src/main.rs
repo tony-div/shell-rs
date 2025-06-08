a3904ff2f5e6b2943da18a1c7201b3f534b8b701
Tony George <tg764562@gmail.com>
2025-06-08 19:53:03 +0300

    chore: refactor builtin commands

--- src/main.rs
+++ src/main.rs
@@ -9,37 +9,35 @@ fn main() {
         stdout.flush().unwrap();
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
-        let mut splitted_line = input.split_whitespace();
-        let command = splitted_line.next().unwrap();
-        if command == "exit" {
-            let state: i32 = splitted_line.next().unwrap().trim().parse().unwrap();
-            std::process::exit(state);
-        } else if command == "echo" {
-            loop {
-                let next_param = splitted_line.next();
-                if next_param == None {
-                    break;
-                }
-                print!("{} ", next_param.unwrap());
-            }
-            println!("\u{8}");
-        } else if command == "type" {
-            let builtin = ["exit", "echo", "type"];
-            let next_param = splitted_line.next().unwrap();
-            let mut found = false;
-            for builtin_command in builtin {
-                if builtin_command == next_param {
-                    println!("{builtin_command} is a shell builtin");
-                    found = true;
-                    break;
-                }
-            }
-            if found == false {
-                println!("{next_param}: not found");
-            }
-        }
-        else {
-            println!("{}: command not found", command);
+        let command = input.trim().split(" ").collect::<Vec<&str>>();
+        match command.as_slice() {
+            [""] => continue,
+            ["exit", args @ ..] => exit_cmd(args),
+            ["echo", args @ ..] => echo_cmd(args),
+            ["type", args @ ..] => type_cmd(args),
+            other => println!("{}: command not found", other[0]),
         }
     }
+}
+
+fn exit_cmd(args: &[&str]) {
+    std::process::exit(args[0].parse().unwrap())
+}
+
+fn echo_cmd(args: &[&str]) {
+    for arg in args {
+        print!("{arg} ");
+    }
+    println!("\u{8}");
+}
+
+fn type_cmd(args: &[&str]) {
+    let builtin = ["exit", "echo", "type"];
+    for builtin_command in builtin {
+        if builtin_command == args[0] {
+            println!("{builtin_command} is a shell builtin");
+            return;
+        }
+    }
+    println!("{}: not found", args[0]);
 }
